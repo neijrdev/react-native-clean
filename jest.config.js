39de85fe96e445ccdcc642f861e8dc88f93ecd9e
neijunior <n.junior@palivre.com>
2024-08-12 19:58:41 -0300

    add config jest config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
+  roots: ['<rootDir>/__tests__'],
   preset: 'react-native',
+  testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
 };
